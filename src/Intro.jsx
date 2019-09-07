fce5edda4d78da83ef8ed79d7eb8cf66e73f2dd1
Yash Shah <shahhyash@outlook.com>
2019-09-07 01:13:10 -0400

    Fixed issue where particles background disabled buttons

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -25,19 +25,19 @@ export class IntroComponent extends React.Component {
                     <h2 className="alt"><strong>Yash Shah</strong></h2>
                     <p>Hi, I'm a student majoring in Computer Engineering & Computer Science at Rutgers University. I'm incredibly passionate about technology and entrepreneurship and I'm constantly working towards gaining experience in both fields.</p>
                     <Row noGutters>
-                        <Button variant="outline-light" className="mt-2 mr-2" href="mailto:yash.shah830@rutgers.edu">
+                        <Button variant="outline-light" className="mt-2 mr-2" href="mailto:yash.shah830@rutgers.edu" style={{zIndex: 99}}>
                             <FontAwesomeIcon className="mr-2" icon={faEnvelope} />
                             Email
                         </Button>
-                        <Button variant="outline-light" className="mt-2 mr-2" href="/files/resume.pdf">
+                        <Button variant="outline-light" className="mt-2 mr-2" href="/files/resume.pdf" style={{zIndex: 99}}>
                             <FontAwesomeIcon className="mr-2" icon={faFileAlt} />
                             Resume
                         </Button>
-                        <Button variant="outline-light" className="mt-2 mr-2" href="https://github.com/shahhyash">
+                        <Button variant="outline-light" className="mt-2 mr-2" href="https://github.com/shahhyash" style={{zIndex: 99}}>
                             <FontAwesomeIcon className="mr-2" icon={faGithub} />
                             Github
                         </Button>
-                        <Button variant="outline-light" className="mt-2" href="https://www.linkedin.com/in/shahhyash">
+                        <Button variant="outline-light" className="mt-2" href="https://www.linkedin.com/in/shahhyash" style={{zIndex: 99}}>
                             <FontAwesomeIcon className="mr-2" icon={faLinkedin} />
                             LinkedIn
                         </Button>
